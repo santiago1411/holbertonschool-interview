142a790e4c6f76ea6b6b8aa5e788271fd6438ab6
santiago1411 <santy.osorno2@gmail.com>
2023-04-09 16:33:06 -0500

    lockboxes

---
+++ 0-lockboxes.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""method that determines if all the boxes can be opened."""
+
+
+def canUnlockAll(boxes):
+    # """se recorren las posiciones de las cajas que se pueden abrir"""
+    # for j in range(len(boxes)):
+    #     if boxes[j]:
+    #         continue
+    #     else:
+    #         if (j < len(boxes) - 1):
+    #             return False
+    # return True
+
+    lista_v = [0]
+    for i in lista_v:
+        for j in boxes[i]:
+            if j < len(boxes) and j not in lista_v:
+                lista_v.append(j)
+        if len(lista_v) == len(boxes):
+            return True
+    return False
